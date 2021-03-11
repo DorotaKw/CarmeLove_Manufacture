18f5d44275dc8cb0f4047e534fc2c6888b7730d0
Dorota Chroboczek <dorotachroboczek@gmail.com>
2021-03-11 19:32:06 +0100

    feature(cl): implement forms & views for accounts, sign_up needs to be fix

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+from .views import *
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', SubmittableLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('password_change/', SubmittablePasswordChangeView.as_view(),
+         name='password_change'),
+    path('sign_up/', SignUpView.as_view(), name='sign_up'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import SignUpForm
+
+
+class SubmittableLoginView(LoginView):
+    template_name = 'login_form.html'
+
+
+class SubmittablePasswordChangeView(PasswordChangeView):
+    template_name = 'login_form.html'
+    success_url = reverse_lazy('home')
+
+
+class SignUpView(CreateView):
+    template_name = 'login_form.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('home')
